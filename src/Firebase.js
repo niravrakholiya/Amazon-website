91d7159a1a83d1ec2fc733fc9c3386e4eb7df35c
niravrakholiya <124346628+niravrakholiya@users.noreply.github.com>
2024-03-13 10:15:36 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkvRhPdHVbfIn6lgGDp5JpN6IEsJZbfVM",
+  authDomain: "addpost-b3ab5.firebaseapp.com",
+  projectId: "addpost-b3ab5",
+  storageBucket: "addpost-b3ab5.appspot.com",
+  messagingSenderId: "1063396162236",
+  appId: "1:1063396162236:web:37fbf09fc35d3a0d7aaeb9",
+  measurementId: "G-0H90RS8E5J"
+};
+export const app = initializeApp(firebaseConfig);
